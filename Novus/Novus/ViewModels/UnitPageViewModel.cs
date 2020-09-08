0f32c7865525d2bc56d445cfb29ce2549b06a1b8
Harry <hheslin@gmail.com>
2020-09-09 08:54:32 +1000

    First commit

---
+++ Novus/Novus/ViewModels/UnitPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using MvvmHelpers;
+namespace Novus.ViewModels
+{
+    class UnitPageViewModel : BaseViewModel
+    {
+        public UnitPageViewModel()
+        {
+            ChangeUnit = new Command(NextUnit);
+        }
+
+        string[] units = { "IAB330", "CAB432", "CAB303"};
+        int unitIndex = 0;
+        public string UnitName => units[unitIndex];
+
+       public Command ChangeUnit { get; }
+
+        void NextUnit()
+        {
+            if (unitIndex < 2)
+            {
+                unitIndex++;
+            }
+            else
+            {
+                unitIndex = 0;
+            }
+            OnPropertyChanged(nameof(UnitName));
+        }
+    }
+}

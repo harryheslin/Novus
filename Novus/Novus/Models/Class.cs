3ac00e8f74a4c59e427a4003a47543820c7aacb8
Kealan <kealandunnett@gmail.com>
2020-09-23 13:38:45 +1000

    Front end design of Register and Sign-On Screens
    Screen designs are made with very basic data binding added to them. Next step will be to add two way data binding and a dynamic display.

---
+++ Novus/Novus/Models/Class.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telerik.Windows.Documents.Spreadsheet.Expressions.Functions;
+
+namespace Novus.Models
+{
+    enum ClassType
+    {
+        Lecture = 1,
+        Tutorial = 2,
+        Practical = 3
+    }
+    enum ClassMode
+    {
+        Physical = 1,
+        Virtual = 2
+
+    }
+
+    class Class
+    {
+
+        public ClassType Type { get; private set; }
+        public DayOfWeek DayOfWeek { get; private set; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public ClassMode Mode { get; private set; }
+        public string Room { get; private set; }
+        public Class(ClassType type, DayOfWeek dayOfWeek,DateTime startTime, DateTime endTime, ClassMode mode, string room)
+        {
+            this.Type = type;
+            this.DayOfWeek = dayOfWeek;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.Room = room;
+        }
+
+        public static Class[] GenerateClassLecture(int returnArrayLength)
+        {
+            Class[] returnArray = new Class[returnArrayLength];
+            for(int i = 0; i < returnArrayLength; i++) {
+                returnArray[i] = new Class(ClassType.Lecture, DayOfWeek.Monday, DateTime.Now, DateTime.Now, ClassMode.Virtual, "");
+            }
+            return returnArray;
+        }
+
+        public static Class[] GenerateClassTutorial( int returnArrayLength)
+        {
+            Class[] returnArray = new Class[returnArrayLength];
+            for (int i = 0; i < returnArrayLength; i++)
+            {
+                returnArray[i] = new Class(ClassType.Tutorial, DayOfWeek.Monday, DateTime.Now, DateTime.Now, ClassMode.Virtual, "");
+            }
+            return returnArray;
+        }
+    }
+}
---
+++ Novus/Novus/Views/Register.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Novus.Models;
+using Novus.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Novus.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var unit = e.Item as Unit;
+            var vm = BindingContext as RegisterViewModel;
+            vm?.ShowOrHideUnit(unit);
+        }
+    }
+}

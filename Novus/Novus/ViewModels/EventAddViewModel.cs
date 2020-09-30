0de8ce15ee2ee28fb5ea75dfe72d1b1975f20a92
lagstar4 <61819558+lagstar4@users.noreply.github.com>
2020-09-30 21:31:39 +1000

    updated view models used

--- Novus/Novus/ViewModels/EventAddViewModel.cs
+++ Novus/Novus/ViewModels/EventAddViewModel.cs
@@ -9,42 +9,6 @@ using Xamarin.Forms;
 namespace Novus.ViewModels
 {
     class EventAddViewModel : BaseViewModel
-    {
-        public EventAddViewModel()
-        {
-            var date = DateTime.Today;
-            this.Appointments = new ObservableCollection<Appointment>
-            {
-            new Appointment {
-                    Title = "Meeting with Tom",
-                    Detail = "Sea Garden",
-                    StartDate = DateTime.Parse("23/12/2020").AddHours(0),
-                    EndDate = DateTime.Parse("23/12/2020").AddHours(0),
-                    Color = Color.Tomato,   
-                    IsAllDay=true
-                },
-                new Appointment {
-                    Title = "Lunch with Sara",
-                    Detail = "Restaurant",
-                    StartDate = date,
-                    EndDate = date.AddHours(14),
-                    Color = Color.DarkTurquoise
-                },
-                new Appointment {
-                    Title = "Elle Birthday",
-                    StartDate = date,
-                    EndDate = date.AddHours(11),
-                    Color = Color.Orange,
-                    IsAllDay = true
-                },
-                 new Appointment {
-                    Title = "Football Game",
-                    StartDate = date.AddDays(2).AddHours(15),
-                    EndDate = date.AddDays(2).AddHours(17),
-                    Color = Color.Green
-                }
-            };
-        }
-        public ObservableCollection<Appointment> Appointments { get; set; }
+    {                                      
     }
 }
---
+++ Novus/Novus/ViewModels/EventsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+using Telerik.XamarinForms.Input;
+
+namespace Novus.ViewModels
+{
+    class EventsViewModel : BaseViewModel
+    {
+        public EventsViewModel()
+        {
+            Appointments = new ObservableCollection<Appointment>
+            {
+                new Appointment
+                {
+                    Title = "Yeet",
+                    Detail = "Tis the Day",
+                    StartDate = DateTime.Parse("21/09/2020"),
+                    EndDate = DateTime.Parse("21/09/2020"),
+                    Color = Color.Red,
+                    IsAllDay = true
+                }
+            };
+        }
+        public ObservableCollection<Appointment> Appointments { get; set; }
+    }
+}

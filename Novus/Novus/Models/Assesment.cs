881ebf9cdd17a9c36711239cc030eba2879ee252
Harry <hheslin@gmail.com>
2020-10-06 18:14:30 +1000

    Completed Grades View with mock data

--- Novus/Novus/Models/Assesment.cs
+++ Novus/Novus/Models/Assesment.cs
@@ -16,13 +16,17 @@ namespace Novus.Models
         public string DueDate { get; private set; }
         public bool Graded { get; private set; }
         public string GradedDate { get; private set; }
-        public Assesment(string code, string title, int percentage, string releaseDate, string dueDate)
+        public string Grade { get; private set; }
+        public Assesment(string code, string title, int percentage, string releaseDate, string dueDate, bool graded, string gradedDate, string grade)
         {
             this.Code = code;
             this.Title = title;
             this.Percentage = percentage.ToString() + "%";
             this.ReleaseDate = releaseDate;
             this.DueDate = dueDate;
+            this.Graded = graded;
+            this.GradedDate = gradedDate;
+            this.Grade = grade;
         }
 
         public static Assesment[] GenerateAssesments(string unitCode, int returnArrayLength)
@@ -30,7 +34,17 @@ namespace Novus.Models
             Assesment[] returnArray = new Assesment[returnArrayLength];
             for(int i = 0; i< returnArrayLength; i++)
             {
-                returnArray[i] = new Assesment(unitCode + i.ToString(), unitCode + " Test Assignment", 30, DateTime.MinValue.ToShortDateString(), DateTime.Now.ToShortDateString());
+                returnArray[i] = new Assesment(unitCode + i.ToString(), unitCode + " Test Assignment", 30, DateTime.MinValue.ToShortDateString(), DateTime.Now.ToShortDateString(), false, "TBC", "0");
+            }
+            return returnArray;
+        }
+
+        public static Assesment[] GenerateCompleteAssesment(string unitCode, int returnArrayLength)
+        {
+            Assesment[] returnArray = new Assesment[returnArrayLength];
+            for (int i = 0; i < returnArrayLength; i++)
+            {
+                returnArray[i] = new Assesment(unitCode + i.ToString(), unitCode + " Assignment", 30, DateTime.MinValue.ToShortDateString(), DateTime.Now.ToShortDateString(), true, DateTime.Now.ToShortDateString().ToString(), "35/40");
             }
             return returnArray;
         }

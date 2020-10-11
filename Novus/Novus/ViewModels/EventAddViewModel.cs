1aa9c799d0845d9854d51107f2b8c4ddffa0280d
lagstar4 <61819558+lagstar4@users.noreply.github.com>
2020-10-11 15:44:36 +1000

    updated the colour finder and deleted unused paramenters

--- Novus/Novus/ViewModels/EventAddViewModel.cs
+++ Novus/Novus/ViewModels/EventAddViewModel.cs
@@ -2,6 +2,7 @@
 using Novus.Models;
 using MvvmHelpers;
 using Xamarin.Forms;
+using System.ComponentModel;
 
 namespace Novus.ViewModels
 {
@@ -13,28 +14,28 @@ namespace Novus.ViewModels
         public string NameInput { set; get; }
         public string DescriptionInput { set; get; }
         public Command AddEventButton { get; }
-        DateTime startDateSelected;
+        private DateTime startDateSelected;
         public DateTime StartDateSelected
         {
-            get; set;
-            //get=> startDateSelected;
-            //set
-            //{            
-            //    SetProperty(ref startDateSelected, value);
-            //    OnPropertyChanged(nameof(StartDateSelected));
-            //}
+            //get; set;
+            get => startDateSelected;
+            set
+            {
+                SetProperty(ref startDateSelected, value);
+                OnPropertyChanged(nameof(StartDateSelected));
+            }
         }
 
-        //DateTime endDateSelected;
+        DateTime endDateSelected;
         public DateTime EndDateSelected
         {
-            get; set;
-            //get=> endDateSelected;
-            //set
-            //{
-            //    SetProperty(ref endDateSelected, value);
-            //    OnPropertyChanged(nameof(StartDateSelected));
-            //}
+            //get; set;
+            get => endDateSelected;
+            set
+            {
+                SetProperty(ref endDateSelected, value);
+                OnPropertyChanged(nameof(EndDateSelected));
+            }
         }
         public string ColourSelected { get; set; }
         public bool AllDayToggle { set; get; }
@@ -45,15 +46,28 @@ namespace Novus.ViewModels
         public EventAddViewModel()
         {
             AddEventButton = new Command(AddEventAndGoBack);
-            Today = DateTime.Now;
+            Today = DateTime.Today;
             ColourSelected = "Red";
         }
 
         async void AddEventAndGoBack()
-        {
-            Events.AddToEvents(NameInput, DescriptionInput, StartDateSelected, EndDateSelected, ColourSelected, AllDayToggle);
-            await Shell.Current.Navigation.PopAsync();
-            //Console.WriteLine(EndDateSelected);
+        {            
+            if (EndDateSelected < StartDateSelected)
+            {
+                await Application.Current.MainPage.DisplayAlert("Check your dates!", "Please ensure the end date is after the start date","OK");
+            }
+            else if (EndDateSelected==StartDateSelected && !AllDayToggle)
+            {
+                await Application.Current.MainPage.DisplayAlert("Check your dates!","An event can not be zero minutes long. please selected a later" +
+                    "date or turn the all day slider on","OK");
+            }
+            else
+            {
+                Events.AddToEvents(NameInput, DescriptionInput, StartDateSelected, EndDateSelected, ColourSelected, AllDayToggle);
+                await Shell.Current.Navigation.PopAsync();
+            }
         }
+
+
     }
 }

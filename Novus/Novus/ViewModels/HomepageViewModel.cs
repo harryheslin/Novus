ae2f42ac13cb382a08062d4b3e50aef1cdbf8e1c
lagstar4 <61819558+lagstar4@users.noreply.github.com>
2020-10-22 13:20:43 +1000

    added the next event to the home page

--- Novus/Novus/ViewModels/HomepageViewModel.cs
+++ Novus/Novus/ViewModels/HomepageViewModel.cs
@@ -1,23 +1,26 @@
 ﻿using MvvmHelpers;
 using Novus.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Novus.ViewModels
 {
-    class HomepageViewModel : BaseViewModel 
+    class HomepageViewModel : BaseViewModel
     {
+
         ObservableCollection<Unit> currentUnits = App.Student.CurrentUnits;
+       
+        ObservableCollection<Events> CurrentEvents = App.Student.Events;
+
+
 
         Unit announcementUnit = App.Student.CurrentUnits[0];
         public Command OpenMyUnitsPage { get; }
         public Command CalendarPage { get; }
         public Command EmailPage { get; }
         public Command AnnouncementUnitPage { get; }
+        public Command CalendarWeekPage { get; }
 
         public HomepageViewModel()
         {
@@ -25,7 +28,10 @@ namespace Novus.ViewModels
             CalendarPage = new Command(GoToCalendarPage);
             EmailPage = new Command(GoToEmailPage);
             AnnouncementUnitPage = new Command(GoToAnnouncementUnitPage);
+            CalendarWeekPage = new Command(GoToCalendarWeekPage);
             GetLatestAnnouncement();
+
+            GetLatestEvents();
         }
 
         private void GetLatestAnnouncement()
@@ -46,6 +52,41 @@ namespace Novus.ViewModels
             }
             announcementUnit = latestUnit;
             LatestAnnouncement = latest;
+        }
+
+        private void GetLatestEvents()
+        {
+
+            if (CurrentEvents.Count == 0)
+            {
+                latestEventName = "No Events";
+                latestEventDate = "00/00/0000";
+                latestEventDescription = "No Descrpition";
+            }
+            else
+            {
+                Events latestEvents = CurrentEvents[0];
+                for (int i = 0; i < CurrentEvents.Count; i++)
+                {
+                    if (CurrentEvents[i].StartDate > latestEvents.StartDate)
+                    {
+                        latestEvents = CurrentEvents[i];
+                    }
+                }
+                LastestEvent = latestEvents;
+                latestEventName = LastestEvent.EventName;
+                latestEventDate = LastestEvent.StartDate.ToString("dd/MM/yyyy");
+                if (LastestEvent.EventDescription == null)
+                {
+                    latestEventDescription = "No Descrpition";
+                }
+                else
+                {
+                    latestEventDescription = LastestEvent.EventDescription;
+                }
+
+            }
+
         }
 
         Announcement latestAnnouncement;
@@ -75,6 +116,53 @@ namespace Novus.ViewModels
             }
         }
 
+        Events latestEvent;
+        public Events LastestEvent
+        {
+            get => latestEvent;
+            set
+            {
+                SetProperty(ref latestEvent, value);
+                OnPropertyChanged(nameof(LastestEvent));
+            }
+        }
+
+        string latestEventDate;
+        public string LatestEventDate
+        {
+            get => latestEventDate;
+            set
+            {
+                SetProperty(ref latestEventDate, value);
+                OnPropertyChanged(nameof(LatestEventDate));
+            }
+
+        }
+
+        string latestEventName;
+        public string LatestEventName
+        {
+            get => latestEventName;
+            set
+            {
+                SetProperty(ref latestEventName, value);
+                OnPropertyChanged(nameof(LatestEventName));
+            }
+        }
+
+        string latestEventDescription;
+        public string LatestEventDescription
+        {
+            get => latestEventDescription;
+            set
+            {
+                SetProperty(ref latestEventDescription, value);
+                OnPropertyChanged(nameof(LatestEventDescription));
+            }
+        }
+
+
+
         async void GoToAnnouncementUnitPage(object s)
         {
             string param = s.ToString();
@@ -97,6 +185,11 @@ namespace Novus.ViewModels
         async void GoToCalendarPage()
         {
             await Shell.Current.GoToAsync($"calendarHome");
+        }
+
+        async void GoToCalendarWeekPage()
+        {
+            await Shell.Current.GoToAsync("calendarWeek");
         }
 
         async void GoToEmailPage()
